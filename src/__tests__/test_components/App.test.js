bf57fd7c0baca6e26650e64f6da73d1e76914154
jose <jennymarin1989@hotmail.com>
2018-04-26 20:22:57 +0100

    :construction: Install enzyme adaptor

---
+++ src/__tests__/test_components/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../components/App';
+
+
+
+describe('App', () => {
+	let app = shallow(<App />);
+
+	it('renders correctly', () => {
+		expect(app).toMatchSnaptshot();
+	});	
+	
+});
