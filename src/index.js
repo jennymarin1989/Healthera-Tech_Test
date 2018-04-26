24aa694fb04a4fffc498f9e3d5e12b662399e423
jose <jennymarin1989@hotmail.com>
2018-04-26 23:04:19 +0100

    :bug: Problem to render App component in localhost - Fix Bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import render from 'react-dom';
+import ReactDom from 'react-dom';
 import App from './components/App';
 
 
-render(<App />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
