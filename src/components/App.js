6b0746f5c1adeb79e6f562fe1e3be87b2ff7964c
jose <jennymarin1989@hotmail.com>
2018-04-27 14:05:49 +0100

    Fail test, haven't created a  header component

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     render(){
         return(
             <div>
-               <h1>HEALTHERA</h1> 
+               <Header /> 
             </div>
         ) 
     }
