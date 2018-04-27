c028e41c1f07b7d30a6c686b3f0c74711ebd8e62
jose <jennymarin1989@hotmail.com>
2018-04-27 14:09:34 +0100

    :construction: Add test for Header component

---
+++ src/__tests__/test_components/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../components/Header';
+
+describe('Header', () => {
+
+    let header = shallow(<Header />);
+
+  it('renders correctly', () =>{
+		expect(header).toMatchSnapshot();
+	})
+
+})
