0aa839e6231f8d45cf97c735b48ed27deeb48515
jose <jennymarin1989@hotmail.com>
2018-04-27 18:26:05 +0100

    :construction: Add test for SubmitButton component

---
+++ src/__tests__/SubmitButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SubmitButton from '../../components/SubmitButton';
+
+describe("SubmitButton", () => {
+	
+	let submit = shallow(<SubmitButton />);
+
+	it("renders correctly", () => {
+		expect(submit).toMatchSnapshot();
+	});
+
+	describe('When a user clicks a button', () => {
+		it('should call the handleDataSumittedButton function', ()=>{
+			let mockDataSubmitted = jest.fn()
+			let submit = shallow(<SubmitButton handleDataSubmitted = {mockDataSubmitted}/>);
+			submit.find('#submittedButton').simulate('click');
+			expect(mockDataSubmitted).toBeCalled();
+		})
+	});
+});
