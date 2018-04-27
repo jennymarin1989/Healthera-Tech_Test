c033cd040b531cabda5811598e84d062cf19062d
jose <jennymarin1989@hotmail.com>
2018-04-27 14:02:35 +0100

    :construction: Add test for rendering Header components

--- src/__tests__/test_components/App.test.js
+++ src/__tests__/test_components/App.test.js
@@ -3,11 +3,14 @@ import { shallow } from 'enzyme';
 import App from '../../components/App';
 
 
-
 describe('App', () => {
 	let app = shallow(<App />);
 
 	it('renders correctly', () => {
 		expect(app).toMatchSnapshot();
 	});	
+
+	describe('Rendering components', () => {
+		expect(app.find('Header').exists()).toBe(true);
+	})
 });
