3303c41a16831cc4edb75f1501cfc8bb316e7f68
jose <jennymarin1989@hotmail.com>
2018-04-29 00:35:38 +0100

    :construction: Add test for rendering Message component in Patients.js

--- src/__tests__/test_components/Patients.test.js
+++ src/__tests__/test_components/Patients.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Patients from '../../components/Patients';
+import Message from '../../components/Messages';
 
 describe("Patients", () => {
 
-    let patients = shallow(<Patients />)
+	let patients = shallow(<Patients />)
 
    it('renders components correctly',() =>{
        expect(patients).toMatchSnapshot();
@@ -22,13 +23,18 @@ describe("Patients", () => {
  describe("renders correctly", () => {
    it("render header component correctly", () => {
        expect(patients.find('Header').exists()).toBe(true);
-   });
+	 });
+	 
+	 it('render Message component correctly', () => {
+		 expect(patients.find('Message').exists()).toBe(true);
+	 })
  });
 
  describe("initializes state successfully", () => {
 	it('initializes selectedOption as false', () => {
 		expect(patients.state('selectedOption')).toEqual("");
-	})
+	});
+  });	
 
 	describe("Updates states successfully", () => {
 		it("changes state of selectedOption from 0 to option", () => {
@@ -43,7 +49,6 @@ describe("Patients", () => {
 	describe("renders dropdown component successfully" ,() => {
 		it("renders dropdown with patients name successfylly", () => {
 			expect(patients.find('Dropdown').exists()).toBe(true);
-		})
+		});
 	})	
- });
 });
