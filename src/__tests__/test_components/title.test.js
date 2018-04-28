6db459dd53c904790508cf473f60648f99ccb79e
jose <jennymarin1989@hotmail.com>
2018-04-28 18:19:43 +0100

    :white_check_mark: Pass test for title styled component

--- src/__tests__/test_components/title.test.js
+++ src/__tests__/test_components/title.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import render from 'react-test-renderer';
+import Title from '../../components/styling/title';
 import 'jest-styled-components';
 
 describe('title', () => {
     let title = render.create(<Title />).toJSON();
   it("returns title with specific style", () => {
      expect(title).toMatchSnapshot()
-     expect(title).toHaveStyleRule('color', 'green') 
+     expect(title).toHaveStyleRule('color', 'MediumSeaGreen') 
   })  
 
 })
---
+++ src/components/styling/title.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Title = styled.text `
+    font-size: 1.875em;
+    color: MediumSeaGreen;
+    font-family: verdana;
+`;
+
+export default Title;
