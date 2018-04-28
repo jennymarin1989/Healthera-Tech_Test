0066430b60bd57ee0dc6deb8fa23bc9fab5958f3
jose <jennymarin1989@hotmail.com>
2018-04-28 18:23:38 +0100

    :construction: update test in header components - render Title styled component

--- src/__tests__/test_components/Header.test.js
+++ src/__tests__/test_components/Header.test.js
@@ -5,9 +5,13 @@ import Header from '../../components/Header';
 describe('Header', () => {
 
     let header = shallow(<Header />);
-
+    
   it('renders correctly', () =>{
 		expect(header).toMatchSnapshot();
-	})
+  })
 
+describe('renders component correctly', ()=>{
+   expect(header.find('Title').exists()).toBe(true);
 })
+  
+});
