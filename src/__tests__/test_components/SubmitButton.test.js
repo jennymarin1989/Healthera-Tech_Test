d74db123ffd370421e62985f50041ec44437cacd
jose <jennymarin1989@hotmail.com>
2018-05-05 07:25:53 +0100

    ::package:Add npm sinon to mock and spy functions easily - Trying to fix test of SubmitButton component, should call parent function when clicked

--- src/__tests__/test_components/SubmitButton.test.js
+++ src/__tests__/test_components/SubmitButton.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import SubmitButton from '../../components/SubmitButton';
+import App from '../../components/App.js'
 
 describe("SubmitButton", () => {
 	
@@ -15,14 +16,14 @@ describe("SubmitButton", () => {
 		expect(submit).toMatchSnapshot();
 	});
 
-	// describe('When a user clicks a button', () => {
-	// 	it('should call the handleDataSumittedButton function', ()=>{
-	// 		let mockhandlePortalClicked = jest.fn()
-	// 		let submit2 = shallow(<SubmitButton handlePortalClicked = {mockhandlePortalClicked}/>);
-	// 		submit.find('#submitButton').simulate('click', 'using prototype');
-	// 		expect(mockhandlePortalClicked).toBeCalled();
-	// 	})
-	// });
+	describe('When a user clicks a button', () => {
+		it('should call the handleDataSumittedButton function', ()=>{
+			let mockhandlePortalClicked = jest.fn()
+			let submit2 = shallow(<App handlePortalClicked = {mockhandlePortalClicked}/>);
+			submit2.find('SubmitButton').simulate('click', 'using prototype');
+			expect(mockhandlePortalClicked).toBeCalled();
+		})
+	});
 
 	describe("renders component correctly", () => {
 		it("renders PortalButton component correctly", () => {
